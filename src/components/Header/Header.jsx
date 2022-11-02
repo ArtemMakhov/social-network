cb05aa00646e8ec9433bf62495be228a2fccb3cc
Artem Makhov <makhovartem23@gmail.com>
2022-11-02 12:36:05 +0200

    components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+ const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://w7.pngwing.com/pngs/567/579/png-transparent-logo-emblem-brand-design-emblem-text-team.png' width="30" height="30"/>
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="hero" width="100%" height="200" />
+            </div>
+            <div>
+                <img src="https://beebom.com/wp-content/uploads/2022/02/Featured.jpg?w=730&h=487&crop=1&quality=75" alt="avatar" width="150" height="150" />
+            </div>
+            <MyPosts/>
+        </div>
+    )
+};
+
+export default Profile;
