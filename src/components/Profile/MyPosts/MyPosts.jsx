012c9345f5d1b59cbd87ebd58c269303ea818ed9
Artem Makhov <makhovartem23@gmail.com>
2022-11-03 14:44:35 +0200

    wath props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,9 +10,9 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div >
-                <Post />
-                <Post />
-                <Post />
+                <Post message="Hi, how are you?" like="20"/>
+                <Post message="It's my first post" like="12"/>
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src='https://cdn.vox-cdn.com/thumbor/LWlI3ImRc5l27CTiBR7ihrPq6RU=/0x0:1080x718/1400x933/filters:focal(477x288:649x460):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/71263353/300017093_10114630004939621_5854109382330704814_n.0.jpg' alt='avatar' width="50" height="50" />
-            post 1
+            {props.message}
             <div>
-                <span>Like</span>
+                <span>Like - { props.like}</span>
             </div>
         </div>
     )
