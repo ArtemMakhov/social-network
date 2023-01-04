82feb0adaef1b5333afdf65357446dba0c5a8256
Artem Makhov <makhovartem23@gmail.com>
2023-01-04 16:18:50 +0200

    54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,11 +3,10 @@ import axios from 'axios';
 import userPhoto from '../../images/avatar.png';
 
 class Users extends React.Component{
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(responce => this.props.setUsers(responce.data.items));
-    };
+    }
 
   render() {
   return (
