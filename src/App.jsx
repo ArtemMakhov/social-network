40056dac731ef8d7ea4e759f36b74ce74b9e8c73
Artem Makhov <makhovartem23@gmail.com>
2023-03-06 20:18:06 +0200

    lazy load and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React,{Suspense} from 'react';
 import { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from "react-redux";
@@ -5,17 +6,19 @@ import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './components/GlobalStyle';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer, { withRouter } from './components/Profile/ProfileContainer';
+import  { withRouter } from './components/Profile/ProfileContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/UserSettings/Settings';
 import Friends from './components/Friends/Friends';
 import { Wrapper,Content ,Preloader} from './App.styled';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import Login from './components/Login/Login';
 import { initializeApp } from './redux/app-reduser';
 import { Loader } from './components/common/Loader/Loader';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -28,26 +31,34 @@ class App extends Component {
         </Preloader>)
     }
     return (
-    <Wrapper>
-      <HeaderContainer />
-      <Navbar />
-      <Content>
-        <Routes>
-          <Route path='/profile' element={<ProfileContainer />}>
-            <Route path=":userId" element={<ProfileContainer />} />
-          </Route>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/dialog/*' element={<DialogsContainer />} />
-          <Route path='/users' element={<UsersContainer/>} />
-          <Route path='/news' element={<News />} />
-          <Route path='/music' element={<Music />} />
-          <Route path='/settings' element={<Settings />} />
-          <Route path='/friends' element={<Friends />} />
-        </Routes>
-      </Content>
-      <GlobalStyle />
-    </Wrapper>   
-  );
+      <Wrapper>
+        <HeaderContainer />
+        <Navbar />
+        <Content>
+          <Routes>
+            <Route path='/profile' element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <ProfileContainer />
+              </Suspense>
+            }>
+              <Route path=":userId" element={<ProfileContainer />} />
+            </Route>
+            <Route path='/login' element={<Login />} />
+            <Route path='/dialog/*' element={
+              <Suspense fallback={<div><Loader/></div>}>
+                <DialogsContainer />
+              </Suspense>
+            } />
+            <Route path='/users' element={<UsersContainer />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/friends' element={<Friends />} />
+          </Routes>
+        </Content>
+        <GlobalStyle />
+      </Wrapper>
+    );
 }
 };
 const mapStateToProps = (state) => ({
